b4b8583acb6ef271c3f243c5aff9751a74bb093f
Acke Tom <tommy14221@hotmail.com>
2013-11-21 12:20:34 +0100

    Oefening3 Pythagoras

--- pyth.rb
+++ pyth.rb
@@ -1,6 +1,6 @@
 def pyth(a,b)
 
-	z=Math.sqrt((a**2)+(b**2))
+	z=Math.sqrt((a**2)+(b**2)).round(2)
 
 
 end
@@ -10,13 +10,17 @@ def hoek(x,y)
 
 
 
-	alfa=((Math.atan2(a,b))/(Math::PI))*180
-	beta=((Math.atan2(b,a))/(Math::PI))*180
+	alfa=((Math.atan2(x,y))/(Math::PI))*180
+	beta=((Math.atan2(y,x))/(Math::PI))*180
 
 
 end
 
 
-puts "De schuine zijde is: " , pyth(ARGV[0].to_i, ARGV[1].to_i) ,"/n"
+puts "De schuine zijde is: " , pyth(ARGV[0].to_i, ARGV[1].to_i) 
+puts "De eerste hoek is: ", hoek(ARGV[0].to_i,ARGV[1].to_i)
+puts "De eerste hoek is: ", hoek(ARGV[1].to_i,ARGV[0].to_i)
+
+
 	
 	
